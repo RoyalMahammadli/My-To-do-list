d8c9fa7845758930830b6939bed3e9fda6741d36
Royal Mahammadli <rosqa_17@mail.ru>
2023-09-19 01:08:45 +0400

    js work

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
+
+const box = document.querySelector('.box')
 const inp = document.querySelector('#inp')
+
 document.querySelector('#addBtn').addEventListener('click', (e) => {
-  // e.stopPropagation()
-  // e.preventDefault()
+  e.stopPropagation()
+  e.preventDefault()
   console.log(inp.value);
-  inp.value = ''
+  if (inp.value) {
+    const listItem = document.createElement('li')
+    const listIcon = document.createElement('i')
+    listItem.classList.add('flex')
+    listIcon.addEventListener('click', () => {
+      // listItem.classList.add('none')
+      listItem.remove()
+    })
+    listIcon.classList.add('fa-solid', 'fa-trash')
+    listItem.innerText = inp.value.trim()
+    box.append(listItem, listIcon)
+    listItem.append(listIcon)
+    inp.value = ''
+
+  } else {
+
+
+    confirm('Type anything')
+
+  }
+
+
 })
